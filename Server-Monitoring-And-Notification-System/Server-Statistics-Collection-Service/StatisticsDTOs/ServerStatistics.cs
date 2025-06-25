a47916164defadde48b12c76aff8f4ec842f2e09
Tariq Omar <tariqomar2004@gmail.com>
2025-06-25 21:08:58 +0300

    impelemt Statistics DTO

---
+++ Server-Monitoring-And-Notification-System/Server-Statistics-Collection-Service/StatisticsDTOs/ServerStatistics.cs
@@ -0,0 +1,17 @@
+﻿namespace Server_Statistics_Collection_Service.StatisticsDTOs;
+
+public class ServerStatistics
+{
+    public double MemoryUsage { get; set; }
+    public double AvailableMemory { get; set; }
+    public double CpuUsage { get; set; }
+    public DateTime Timestamp { get; set; }
+
+    public override string ToString()
+    {
+        return $"Time: {Timestamp}\n" +
+               $"Memory Usage: {MemoryUsage} MB\n" +
+               $"Available Memory: {AvailableMemory} MB\n" +
+               $"CPU Usage: {CpuUsage} %";
+    }
+}
