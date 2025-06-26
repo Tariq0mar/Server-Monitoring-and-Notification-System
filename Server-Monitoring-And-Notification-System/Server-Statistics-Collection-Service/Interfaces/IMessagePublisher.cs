f444c2ce74507f6da63736c15230236c54e383e5
Tariq Omar <tariqomar2004@gmail.com>
2025-06-26 12:58:23 +0300

    finish program

--- Server-Monitoring-And-Notification-System/Server-Statistics-Collection-Service/Interfaces/IMessagePublisher.cs
+++ Server-Monitoring-And-Notification-System/Server-Statistics-Collection-Service/Interfaces/IMessagePublisher.cs
@@ -2,5 +2,5 @@
 
 public interface IMessagePublisher
 {
-    
+    public Task<bool> PublishMessage();
 }
--- Server-Monitoring-And-Notification-System/Server-Statistics-Collection-Service/Program.cs
+++ Server-Monitoring-And-Notification-System/Server-Statistics-Collection-Service/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Server_Statistics_Collection_Service.Message_Queue;
+using Server_Statistics_Collection_Service.StatisticsCalculators;
+
+var serverStatisticsCalculator = new ServerStatisticsCalculator();
+
+var publish = new RabbitMQMessagePublisher(serverStatisticsCalculator);
